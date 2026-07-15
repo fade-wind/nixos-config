48c3bfa2082c6ecd0f7b0298a6ca2500f1b7d926
Fadewind <jamz.p14@gmail.com>
2026-07-15 06:48:04 -0400

    Finished keybinds and created noctalia

--- modules/features/home-modules/desktop/niri/keybinds.nix
+++ modules/features/home-modules/desktop/niri/keybinds.nix
@@ -155,5 +155,76 @@ in
     "Mod+Shift+Page_Up".action = move-workspace-up;
     "Mod+Shift+U".action = move-workspace-down;
     "Mod+Shift+I".action = move-workspace-up;
+    "Mod+WheelScrollDown" = {
+      cooldown-ms = 150;
+      action = focus-workspace-down;
+    };
+    "Mod+WheelScrollUp" = {
+      cooldown-ms = 150;
+      action = focus-workspace-up;
+    };
+    "Mod+Ctrl+WheelScrollDown" = {
+      cooldown-ms = 150;
+      action = move-column-to-workspace-down;
+    };
+    "Mod+Ctrl+WheelScrollUp" = {
+      cooldown-ms = 150;
+      action = move-column-to-workspace-up;
+    };
+    "Mod+WheelScrollRight".action = focus-column-right;
+    "Mod+WheelScrollLeft".action = focus-column-left;
+    "Mod+Ctrl+WheelScrollRight".action = move-column-right;
+    "Mod+Ctrl+WheelScrollLeft".action = move-column-left;
+    "Mod+Shift+WheelScrollDown".action = focus-column-right;
+    "Mod+Shift+WheelScrollUp".action = focus-column-left;
+    "Mod+Ctrl+Shift+WheelScrollDown".action = move-column-right;
+    "Mod+Ctrl+Shift+WheelScrollUp".action = move-column-left;
+    "Mod+1".action = focus-workspace 1;
+    "Mod+2".action = focus-workspace 2;
+    "Mod+3".action = focus-workspace 3;
+    "Mod+4".action = focus-workspace 4;
+    "Mod+5".action = focus-workspace 5;
+    "Mod+6".action = focus-workspace 6;
+    "Mod+7".action = focus-workspace 7;
+    "Mod+8".action = focus-workspace 8;
+    "Mod+9".action = focus-workspace 9;
+    # "Mod+Ctrl+1".action = move-column-to-workspace 1;
+    # "Mod+Ctrl+2".action = move-column-to-workspace 2;
+    # "Mod+Ctrl+3".action = move-column-to-workspace 3;
+    # "Mod+Ctrl+4".action = move-column-to-workspace 4;
+    # "Mod+Ctrl+5".action = move-column-to-workspace 5;
+    # "Mod+Ctrl+6".action = move-column-to-workspace 6;
+    # "Mod+Ctrl+7".action = move-column-to-workspace 7;
+    # "Mod+Ctrl+8".action = move-column-to-workspace 8;
+    # "Mod+Ctrl+9".action = move-column-to-workspace 9;
+    "Mod+Tab".action = focus-workspace-previous;
+    "Mod+BracketLeft".action = consume-or-expel-window-left;
+    "Mod+BracketRight".action = consume-or-expel-window-right;
+    "Mod+Comma".action = consume-window-into-column;
+    "Mod+Period".action = expel-window-from-column;
+    "Mod+R".action = switch-preset-column-width;
+    "Mod+Ctrl+R".action = reset-window-height;
+    "Mod+F".action = maximize-column;
+    "Mod+Shift+F".action = fullscreen-window;
+    "Mod+Ctrl+F".action = expand-column-to-available-width;
+    "Mod+C".action = center-column;
+    "Mod+Ctrl+C".action = center-visible-columns;
+    "Mod+Minus".action = set-column-width "-10%";
+    "Mod+Equal".action = set-column-width "+10%";
+    "Mod+Shift+Minus".action = set-window-height "-10%";
+    "Mod+Shift+Equal".action = set-window-height "+10%";
+    "Mod+V".action = toggle-window-floating;
+    "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
+    "Mod+W".action = toggle-column-tabbed-display;
+    # "Print".action = screenshot;
+    # "Ctrl+Print".action = screenshot-screen;
+    # "Alt+Print".action = screenshot-window;
+    "Mod+Escape" = {
+      allow-inhibiting = false;
+      action = toggle-keyboard-shortcuts-inhibit;
+    };
+    "Mod+Shift+E".action = quit;
+    "Ctrl+Alt+Delete".action = quit;
+    "Mod+Shift+P".action = power-off-monitors;
   };
 }
