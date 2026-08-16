12e0d894cdecf123048bbc875bb3c4b8469fa377
Fadewind <jamz.p14@gmail.com>
2026-08-16 18:04:28 -0400

    Added vscode extensions from home-ops

--- modules/features/home-modules/desktop/vscodium.nix
+++ modules/features/home-modules/desktop/vscodium.nix
@@ -12,11 +12,15 @@ in
         jnoortheen.nix-ide
         redhat.vscode-yaml
         ms-kubernetes-tools.vscode-kubernetes-tools
+        irongeek.vscode-env
+        signageos.signageos-vscode-sops
         ]) ++ 
         (with open-vsx; [ 
           navernoedenis.gruvbox-material-icons
           jeanp413.open-remote-ssh
           snrico-moonlight.gruvbox-material-community
+          blueglassblock.better-json5
+          hverlin.mise-vscode
         ]);
       userSettings = {
         "workbench.colorTheme" = "Gruvbox Material Dark";
