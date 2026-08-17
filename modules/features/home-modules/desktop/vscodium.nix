08326af8b1aa85aeb7ebdde6e26e56a67e29287f
Fadewind <jamz.p14@gmail.com>
2026-08-16 21:14:22 -0400

    Added zsh to vscodium

--- modules/features/home-modules/desktop/vscodium.nix
+++ modules/features/home-modules/desktop/vscodium.nix
@@ -23,31 +23,37 @@ in
           hverlin.mise-vscode
         ]);
       userSettings = {
-        "editor.fontFamily" = "'JetBrainsMono Nerd Font','Droid Sans Mono', monospace";
-        "editor.wordWrap" = "on";
-        "workbench.colorTheme" = "Gruvbox Material Dark";
-        "editor.tabSize" = 2;
-        "editor.fontSize" = 16;
+        "chat.agent.enabled" = false;
         "editor.cursorStyle" = "block";
+        "editor.fontFamily" = "'JetBrainsMono Nerd Font','Droid Sans Mono', monospace";
+        "editor.fontSize" = 16;
+        "editor.lineNumbers" = "relative";
         "editor.minimap.enabled" = "false";
-        "workbench.activityBar.compact" = true;
-        "workbench.activityBar.location" = "top";
-        "workbench.iconTheme" = "gruvbox-material-icons";
-        "workbench.preferredDarkColorTheme" = "Gruvbox Material Dark";
-        "workbench.sideBar.location" = "left";
+        "editor.tabSize" = 2;
+        "editor.wordWrap" = "on";
         "redhat.telemetry.enabled" = false;
+        "terminal.integrated.defaultProfile.linux" = "zsh";
+        "terminal.integration.profiles.linux" = {
+          zsh = {
+            path = "${pkgs.zsh}/bin/zsh";
+          };
+        };
+        "vim.smartRelativeLine" = true;
         "vs-kubernetes" = {
             "vs-kubernetes.crd-code-completion" = "enabled";
         };
-        "chat.agent.enabled" = false;
-        "editor.lineNumbers" = "relative";
-        "vim.smartRelativeLine" = true;
         "vscode-kubernetes.helm-path" = "${pkgs.kubernetes-helm}/bin/helm";
         "vscode-kubernetes.kubectl-path" = "${pkgs.kubectl}/bin/kubectl";
-        "workbench.navigationControl.enabled" = false;
-        "window.titleBarStyle" = "custom";
         "window.controlsStyle" = "hidden";
         "window.menuBarVisibility" = "compact";
+        "window.titleBarStyle" = "custom";
+        "workbench.activityBar.compact" = true;
+        "workbench.activityBar.location" = "top";
+        "workbench.colorTheme" = "Gruvbox Material Dark";
+        "workbench.iconTheme" = "gruvbox-material-icons";
+        "workbench.navigationControl.enabled" = false;
+        "workbench.preferredDarkColorTheme" = "Gruvbox Material Dark";
+        "workbench.sideBar.location" = "left";
       };
     };
   };
